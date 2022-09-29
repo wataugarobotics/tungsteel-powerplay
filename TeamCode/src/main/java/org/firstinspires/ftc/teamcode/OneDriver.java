656de787ae82525f5386a8b71d646ca47d1921fc
Sam <samuel.l.nystrom@gmail.com>
2022-09-29 18:24:09 -0400

    Inline commands with InstantCommand

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneDriver.java
@@ -2,10 +2,13 @@ package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.RunCommand;
+import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import org.firstinspires.ftc.teamcode.commands.DriveCommand;
+
 import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.Lift;
 
 /**
  * An opmode for 1 driver in the driver-controlled segment.
@@ -19,7 +22,7 @@ public class OneDriver extends CommandOpMode {
 
         // This will run the command whenever no other command that uses the drivetrain is running,
         // constantly updating the motor powers with the gamepad information.
-        drivetrain.setDefaultCommand(new DriveCommand(drivetrain));
+        drivetrain.setDefaultCommand(new InstantCommand(drivetrain::drive, drivetrain));
         schedule(new RunCommand(telemetry::update));
     }
 }
