79464204f831dde0bc932f2dbe3d9daf5b60eca5
Sam <samuel.l.nystrom@gmail.com>
2022-10-01 12:33:39 -0400

    Reorganize drivetrain logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneDriver.java
@@ -2,10 +2,10 @@ package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.RunCommand;
-import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.commands.DriveCommand;
 import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
 
 /**
@@ -16,11 +16,11 @@ public class OneDriver extends CommandOpMode {
     @Override
     public void initialize() {
         GamepadEx gamepad = new GamepadEx(gamepad1);
-        Drivetrain drivetrain = new Drivetrain(hardwareMap, gamepad);
+        Drivetrain drivetrain = new Drivetrain(hardwareMap);
 
         // This will run the command whenever no other command that uses the drivetrain is running,
         // constantly updating the motor powers with the gamepad information.
-        drivetrain.setDefaultCommand(new InstantCommand(drivetrain::drive, drivetrain));
+        drivetrain.setDefaultCommand(new DriveCommand(drivetrain, gamepad));
         schedule(new RunCommand(telemetry::update));
     }
 }
