8d61f5e4e2e5e458fe41da3c1c69761d2216c919
Sam <samuel.l.nystrom@gmail.com>
2022-10-13 21:21:54 -0400

    Fix joystick directions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DriveCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DriveCommand.java
@@ -10,10 +10,10 @@ import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
  */
 public class DriveCommand extends RunCommand {
     public DriveCommand(Drivetrain drivetrain, GamepadEx gamepad) {
-        super(() -> drivetrain.drive(
-                gamepad.getLeftX(),
+        super(() -> drivetrain.driveFieldCentric(
                 gamepad.getLeftY(),
-                gamepad.getRightX()
+                gamepad.getLeftX(),
+                -gamepad.getRightX()
         ), drivetrain);
     }
 }
